433071e3f89fb9c093ddd88d2747f582738ad439
arjundewani <arjundewani45@gmail.com>
2024-02-24 01:36:29 -0500

    update

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(__name__)  # Corrected from _name_ to __name__
 
 @app.route('/')
 def hello_world():
@@ -9,7 +9,8 @@ def hello_world():
 
 @app.route('/time')
 def current_time():
+    # Returns the current time in a string format
     return datetime.now().strftime("%H:%M:%S")
-
-if __name__ == '__main__':
+    
+if __name__ == '__main__':  # Corrected from _name_ == '__main__'
     app.run(host='0.0.0.0', port=8080, debug=True)
